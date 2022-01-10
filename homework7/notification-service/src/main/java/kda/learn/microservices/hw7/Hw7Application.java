a66028bd7d463a3c07a62187a3292a67892e4413
Кочнев Дмитрий Александрович <Kochnev.D.Alek@sberbank.ru>
2022-01-11 00:50:26 +0400

    HW#7 REST service - Move Notification service to a separate application

---
+++ homework7/notification-service/src/main/java/kda/learn/microservices/hw7/Hw7Application.java
@@ -0,0 +1,13 @@
+package kda.learn.microservices.hw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hw7Application.class, args);
+    }
+
+}
